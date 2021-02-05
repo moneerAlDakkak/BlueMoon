5371f26e79ee9fd1441d7e73d5aeb6204cabdb79
Moneer Al-Dakkak <77343614+moneerAlDakkak@users.noreply.github.com>
2021-02-05 23:37:50 +0200

    Update main.js

--- main.js
+++ main.js
@@ -140,8 +140,6 @@ export function gettingPageReady() {
 
 window.addEventListener('load', gettingPageReady());
 
-window.onresize =_=> {
-    if(document.querySelector('header').classList.contains('full-height-header')) {
-        document.querySelector('header').classList.remove('full-height-header')
-    }
-}
+window.addEventListener('resize', _=> {
+    document.querySelector('header').style.height = 0
+})
